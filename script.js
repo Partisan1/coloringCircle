8cea1f58831ad636fcbeeba4acb98026ee3c041a
Partisan1 <benedekbarnabas.zsolt@gmail.com>
2023-11-10 14:20:16 +0100

    Range

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const button = document.querySelector("button");
+const inputs = document.querySelectorAll("input");
+const red = document.querySelector("input:nth-child(1)");
+const green = document.querySelector("input:nth-child(2)");
+const blue = document.querySelector("input:nth-child(3)");
+const circle = document.querySelector(".circle");
 
+inputs.forEach(inp => {
+    inp.addEventListener("input", () => {
+        circle.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`;
+    })
+})
+
+
+
+
+/*
 button.onclick = () => {
     const red = document.querySelector("input:nth-child(1)");
     const green = document.querySelector("input:nth-child(2)");
@@ -8,6 +23,7 @@ button.onclick = () => {
     
     circle.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`;
 }
+*/
 
 
 
